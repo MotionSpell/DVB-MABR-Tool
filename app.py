acc5be9e8ccfcc5f3cef2ac19301d22aaa24094d
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2025-02-17 11:16:38 -0700

    adjust cache size

--- app.py
+++ app.py
@@ -71,17 +71,17 @@ def main():
             src_filter = src_args['stream_src']
             src = fs.load_src(src_filter)
             dasher = fs.load("dashin:forward=file:split_as")
-
+            manifest_src = ""
         else:
             src = fs.load_src("avgen")
             aenc = fs.load(f"ffenc:c={src_args.get('a_enc', 'aac')}")
             venc = fs.load(f"ffenc:c={src_args.get('v_enc', 'avc')}")
             reframer = fs.load(f"reframer:rt=on")
-            dasher = fs.load("dasher:dmode=dynamic:stl")
+            dasher = fs.load("dasher:dmode=dynamic:stl:tsb=64")
             manifest_src={server_args['manifest_src']}
 
         # Load destination filter for server mode
-        dst_filter_base = f"{server_args['protocol']}{server_args['ip_dst']}:{server_args['port_dst']}/{server_args['manifest_src']}:furl={server_args['fdt_absolute_url']}:carousel={server_args['carousel']}:ifce={server_args['ifce']}:use_inband={server_args['use_inband_transport']}"
+        dst_filter_base = f"{server_args['protocol']}{server_args['ip_dst']}:{server_args['port_dst']}/{manifest_src}:furl={server_args['fdt_absolute_url']}:carousel={server_args['carousel']}:ifce={server_args['ifce']}:use_inband={server_args['use_inband_transport']}"
         dst_filter = (dst_filter_base + (":llmode" if server_args['low_latency'] == "true" else "")
                                         + (f":errsim={server_args['errsim']}" if server_args.get('errsim') else ""))
         dst = fs.load_dst(dst_filter)
@@ -89,7 +89,10 @@ def main():
     
     elif mode == 'gateway':
         # Load source for gateway
-        repair_urls = ",".join(repair_args['repair_urls'].split('\n'))
+        if repair_args['repair'] == "no":
+            repair_urls = ""
+        else:
+            repair_urls = ",".join(repair_args['repair_urls'].split('\n'))
         src_sess_base = f"{gateway_args['protocol']}{gateway_args['ip_src']}:{gateway_args['port_src']}:ifce={gateway_args['ifce']}:repair={repair_args['repair']}:nbcached={gateway_args['nbcached']}"
         src_sess = src_sess_base + (f"::repair_urls={repair_urls}" if repair_urls!="" else "")
         print(src_sess)
@@ -101,7 +104,8 @@ def main():
         dasher= fs.load(dasher_string)
         
         #load http server
-        dst_sess =  f"{gateway_args['ip_addr']}:{gateway_args['port_addr']}/{gateway_args['dst']}:rdirs={gateway_args['rdirs']}:max_cache_size=-{gateway_args['max_cache_size']}:reqlog='*':cors=auto:sutc={gateway_args['sutc']}:max_cache_segs={gateway_args['max_segs']}"
+        #Romain: :max_cache_size=-{gateway_args['max_cache_size']}
+        dst_sess =  f"{gateway_args['ip_addr']}:{gateway_args['port_addr']}/:rdirs={gateway_args['rdirs']}:reqlog='*':cors=auto:sutc={gateway_args['sutc']}:max_cache_segs={gateway_args['max_segs']}"
         print(f"playback link  :  {gateway_args['ip_addr']}:{gateway_args['port_addr']}/{gateway_args['dst']}")
         dst = fs.load_dst(dst_sess)
         gpac.set_logs(gateway_args["logs"])
